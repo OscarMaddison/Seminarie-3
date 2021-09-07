99dc90bb4353ed5cf440ba23d7432f04f038b359
OscarMaddison <83783667+OscarMaddison@users.noreply.github.com>
2021-09-07 17:01:11 +0200

    Add files via upload

---
+++ InvalidItemException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.seminarie3.exceptions;
+
+import com.mycompany.seminarie3.model.Receipt;
+import java.util.*;
+
+/**
+ *
+ * @author Oscar
+ */
+public class InvalidItemException extends Exception{
+    int itemID;
+    
+    public InvalidItemException(int itemID){
+        System.out.print("The product ID '" + itemID + "' is not in the product registry (user)\nThe product ID '" + itemID + "' is not in the product registry (log)\n");
+        }
+}
